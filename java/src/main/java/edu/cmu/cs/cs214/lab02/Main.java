f822fdf92cd2c26e825082f083852d8b410a571c
Andrew <andrewwang12345@gmail.com>
2024-01-23 19:58:33 -0500

    renderer generalized for different shapes

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,12 +1,15 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+import edu.cmu.cs.cs214.lab02.shapes.Circle;
 
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(2, 3);
 
-        Renderer renderer = new Renderer(rectangle);
+        Circle circle = new Circle(3);
+
+        Renderer renderer = new Renderer(circle);
 
         renderer.draw();
     }
